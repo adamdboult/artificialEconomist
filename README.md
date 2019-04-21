57441439b3fbb3bee17a97c6c44cc0705562e7c9
Adam Boult <adamdboult@gmail.com>
2019-04-21 20:14:37 +0100

    Minor fixes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instructions
+Run install.sh
+This will clone the GPT-2 model.
+
+The run scrape.py
+This will scrape PDFs from the FCA.
+
+Then run toText.sh
+This will convert the PDFs to a single TXT file
+
--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-
-
+#########
+# CLONE #
+#########
+echo "Cloning..."
 git clone 'https://github.com/nshepperd/gpt-2.git'
+
+##################
+# DOWNLOAD MODEL #
+##################
+echo "Downloading model..."
 cd gpt-2
 
 python download_model.py 117M
