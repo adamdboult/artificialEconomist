b3a5a14951f20a2ee62a1074226f123b69e17810
Adam Boult <adamdboult@gmail.com>
2019-04-22 23:48:37 +0100

    Fixed training on encoded

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 cd gpt-2/
-sudo PYTHONPATH=src ./train.py --dataset ../merged_file_clean.txt --batch_size 1 --save_every 100 --sample_every 100
+sudo PYTHONPATH=src ./train.py --dataset ../merged_file_clean.txt.npz --batch_size 1 --save_every 100 --sample_every 100
