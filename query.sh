dc184a59021f12db03bded76a1f5a1bb2d2aca23
Adam Boult <adamdboult@gmail.com>
2024-07-29 09:02:14 +0100

    More

--- query.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-import sys
-
-cd ./gpt-2
-
-#sudo python3 ./src/interactive_conditional_samples.py -nsamples 1
-echo "hi"
-#python3 ./src/generate_unconditional_samples.py -nsamples 1
-
