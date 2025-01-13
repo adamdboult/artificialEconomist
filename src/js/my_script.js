ce186dc3bc5273244bc376c5d4ae33c35b0b614f
Adam Boult <adamdboult@gmail.com>
2025-01-13 21:44:08 +0000

    More

---
+++ src/js/my_script.js
@@ -0,0 +1,40 @@
+function waitForStatus(id) {
+	var count = 0;
+	var restartCheck = setInterval(function() {
+		count++;
+		$.ajax({
+			url: "check_answer",
+			type: "POST",
+			data: id,
+			success: function(result) {
+				console.log(count);
+				console.log(result);
+				if (result != "BBnoresponse") {
+					newResult = result.replace(/\n/g, '<br/>')
+					$("#questionResponse").html(newResult);
+					$("#submitButton").show();
+					$("#waitButton").hide();
+					clearInterval(restartCheck);					
+				}	
+			}
+		});
+      }, 10000);
+}
+
+$(document).ready(function(){
+	$("#waitButton").hide()
+	$("#submitButton").click(function(){
+  		var question = document.getElementById("question").value;
+		console.log(question);
+		$("#submitButton").hide();
+		$("#waitButton").show();
+		$("#questionResponseHeader").html(question)
+		$.ajax({type: "POST", url: "submit_question", data: question, success: function(result){
+			console.log("Got initial response!");
+			console.log(result);
+			waitForStatus(result);
+		}});
+		$("#questionResponse").html("<i>Please wait...</i>");
+	});
+});
+
